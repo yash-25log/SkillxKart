f0e6175847dd010dabd4a6decad744383d2ab0ce
YASH <yash003singhal@gmail.com>
2024-05-23 20:30:16 +0530

    [FEAT] User auth

---
+++ src/services/user.service.ts
@@ -0,0 +1,32 @@
+
+import User, { IUser } from "../models/user.model";
+
+import jwt from "jsonwebtoken";
+
+export async function IS_USER_PRESENT(email: string) {
+    try {
+      const user = await User.findOne({ email: email });
+      if (user) {
+        return user;
+      }
+      return null;
+    } catch (error) {
+      
+      throw error;
+    }
+  }
+  
+  export async function CREATE_USER(body: any) {
+    try {
+      const { user,email } = body;
+      console.log("service body =>", body);
+      body.user_meta_id = user._id;
+      const user_data = await User.create(body);
+      return user_data;
+    } catch (error) {
+      
+      throw error;
+    }
+  }
+  
+  
